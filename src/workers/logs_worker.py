b98f0711574d67f14223c7b98ec7e0d67a4bf327
artemetr <my@artemetr.ru>
2021-11-06 03:22:45 +0500

    added _show_rcon_logs_at_the_front

--- src/workers/logs_worker.py
+++ src/workers/logs_worker.py
@@ -11,6 +11,8 @@ def _mock_send(stats):
 
 
 class LogsWorker(threading.Thread):
+    _show_rcon_logs_at_the_front = bool(os.getenv('SHOW_RCON_LOGS_AT_THE_FRONT'))
+
     def __init__(self, flags: WorkersFlags):
         threading.Thread.__init__(self)
         self._flags = flags
@@ -27,7 +29,7 @@ class LogsWorker(threading.Thread):
                     if not self._flags.keep_working or not self._flags.is_server_running and not line:
                         sleep(1)
                         break
-                    if line:
+                    if line and (self._show_rcon_logs_at_the_front or line.find('Thread RCON') == -1):
                         self._send(line)
             except Exception as e:
                 print('LogsWorker::run', e)
